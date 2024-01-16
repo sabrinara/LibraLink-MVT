ddc72d44f0755dac1c9f70312058bc980187fe63
sabrinara <you@example.com>
2024-01-17 00:31:46 +0600

    live link

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://library-management-system-2z2w.onrender.com/', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://library-management-system-2z2w.onrender.com', 'https://*.127.0.0.1']
 
 
 # Application definition
